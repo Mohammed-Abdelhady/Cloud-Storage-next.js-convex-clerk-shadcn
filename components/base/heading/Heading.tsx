ae8e21e1f6e8a9a9464c64a8383b924dd967d467
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-15 08:29:44 +0300

    feat: custom heading component

---
+++ components/base/heading/Heading.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface HeadingProps {
+  size?: "large" | "medium" | "small";
+  tag?: keyof JSX.IntrinsicElements;
+  align?: "left" | "center" | "right";
+  children: React.ReactNode;
+  className?: string;
+}
+
+/**
+ * Renders a heading element with specified size, alignment, and class name.
+ *
+ * @param {Object} props - The properties for the heading.
+ * @param {React.ComponentType} [props.tag="h1"] - The HTML tag for the heading.
+ * @param {"large" | "medium" | "small"} [props.size="large"] - The size of the heading.
+ * @param {"left" | "center" | "right"} [props.align="left"] - The alignment of the heading.
+ * @param {React.ReactNode} props.children - The content of the heading.
+ * @param {string} [props.className=""] - Additional CSS class name for styling.
+ * @return {JSX.Element} The rendered heading element.
+ */
+const Heading = ({
+  tag: Tag = "h1",
+  size = "large", // default size
+  align = "left", // default alignment
+  children,
+  className = "",
+}: HeadingProps) => {
+  const sizeClasses = {
+    large:
+      "text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight sm:leading-snug md:leading-snug lg:leading-normal",
+    medium:
+      "text-xl sm:text-2xl md:text-3xl lg:text-4xl leading-snug sm:leading-normal md:leading-relaxed lg:leading-loose",
+    small:
+      "text-lg sm:text-xl md:text-2xl lg:text-3xl leading-normal sm:leading-relaxed md:leading-loose lg:leading-loose",
+  };
+
+  const alignClasses = {
+    left: "text-left",
+    center: "text-center",
+    right: "text-right",
+  };
+
+  const computedClassName = cn(sizeClasses[size], alignClasses[align], "font-bold", className);
+
+  return <Tag className={computedClassName}>{children}</Tag>;
+};
+
+export default Heading;
