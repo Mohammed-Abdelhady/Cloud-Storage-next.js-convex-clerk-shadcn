b9771c61295a579f55108dbf488bceebafe3c552
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-18 04:13:18 +0300

    feat: cron job for delete old files

---
+++ convex/crons.ts
@@ -0,0 +1,13 @@
+import { cronJobs } from "convex/server";
+import { internal } from "./_generated/api";
+
+const crons = cronJobs();
+
+crons.interval(
+  "delete any old files marked for deletion",
+  { minutes: 1 },
+  //   @ts-ignore
+  internal.files.deleteAllFiles,
+);
+
+export default crons;
