a46c05a7b105c5b0eba7eb958d4a8ea180357059
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-18 05:08:21 +0300

    feat: UploadButton component that handles file upload functionality.

---
+++ app/dashboard/_components/upload-button.tsx
@@ -0,0 +1,181 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
+import { Doc } from "@/convex/_generated/dataModel";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, `Required`),
+});
+
+/**
+ * UploadButton component that handles file upload functionality.
+ *
+ * @param {z.infer<typeof formSchema>} values - The values to be submitted for file upload.
+ * @return {void} This function does not return any value.
+ */
+export function UploadButton() {
+  const { toast } = useToast();
+  const organization = useOrganization();
+  const user = useUser();
+  const generateUploadUrlMutation = useMutation(api.files.generateUploadUrl);
+  const createFileMutation = useMutation(api.files.createFile);
+
+  const [isFileDialogOpen, setIsFileDialogOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+
+  const fileInputRef = form.register("file");
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (!organization.isLoaded || !user.isLoaded) {
+      return;
+    }
+
+    const postUrl = await generateUploadUrlMutation();
+
+    const fileType = values.file[0].type;
+
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": fileType },
+      body: values.file[0],
+    });
+
+    const { storageId } = await result.json();
+
+    const types: Record<string, Doc<"files">["type"]> = {
+      "image/png": "image",
+      "application/pdf": "pdf",
+      "text/csv": "csv",
+    };
+
+    const fileTypeValue = types[fileType];
+
+    if (!fileTypeValue) {
+      return;
+    }
+
+    try {
+      await createFileMutation({
+        name: values.title,
+        fileId: storageId,
+        orgId: (organization.organization?.id ?? user.user?.id) as string,
+        type: fileTypeValue,
+      });
+
+      form.reset();
+      setIsFileDialogOpen(false);
+      1;
+
+      toast({
+        // @ts-ignore
+        variant: "success",
+        title: "File Uploaded",
+        description: "Now everyone can view your file",
+      });
+    } catch (err) {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong",
+        description: "Your file could not be uploaded, try again later",
+      });
+    }
+  };
+
+  return (
+    <Dialog
+      open={isFileDialogOpen}
+      onOpenChange={(isOpen) => {
+        setIsFileDialogOpen(isOpen);
+        form.reset();
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button>Upload File</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="mb-8">Upload your File Here</DialogTitle>
+          <DialogDescription>
+            This file will be accessible by anyone in your organization
+          </DialogDescription>
+        </DialogHeader>
+
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="file"
+                render={() => (
+                  <FormItem>
+                    <FormLabel>File</FormLabel>
+                    <FormControl>
+                      <Input type="file" {...fileInputRef} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={form.formState.isSubmitting} className="flex gap-1">
+                {form.formState.isSubmitting && <Loader2 className="h-4 w-4 animate-spin" />}
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
