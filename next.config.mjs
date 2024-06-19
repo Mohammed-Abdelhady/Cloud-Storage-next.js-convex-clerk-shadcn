94aa81f7c66709d1a694e3932e13493e4c0a779b
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-19 09:13:39 +0300

    fix: clerk hooks installation with .env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_CONVEX_HOSTNAME,
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
