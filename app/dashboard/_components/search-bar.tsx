c87113d924062c9580a98da80cc6ba75142f36ed
Defuse Code <Mohammed_Abdelhady97@hotmail.com>
2024-06-19 09:16:02 +0300

    feat: search files

--- app/dashboard/_components/search-bar.tsx
+++ app/dashboard/_components/search-bar.tsx
@@ -8,9 +8,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import useDebounce from "@/lib/hooks/use-debounce";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2, SearchIcon } from "lucide-react";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -19,47 +20,67 @@ const formSchema = z.object({
 });
 
 /**
- * Renders a search bar component with a form that allows users to input a search query.
+ * Renders a search bar component that allows users to search for file names.
  *
  * @param {Object} props - The component props.
- * @param {string} props.searchQuery - The current search query.
- * @param {function} props.setSearchQuery - A function to update the search query.
+ * @param {string} props.query - The current search query.
+ * @param {function} props.setQuery - The function to update the search query.
  * @return {JSX.Element} The rendered search bar component.
  */
 export function SearchBar({
-  searchQuery,
-  setSearchQuery,
+  query,
+  setQuery,
 }: {
-  searchQuery: string;
-  setSearchQuery: Dispatch<SetStateAction<string>>;
+  query: string;
+  setQuery: Dispatch<SetStateAction<string>>;
 }) {
-  const form = useForm<{ query: string }>({
+  const submitButtonRef = useRef<any>(null);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
-      query: searchQuery,
+      query,
     },
   });
 
-  const onSubmit = async (values: { query: string }) => {
-    setSearchQuery(values.query);
-  };
+  const debouncedSearchTerm = useDebounce<string>(form.watch("query"), 500);
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      submitButtonRef?.current?.click();
+    }
+  }, [debouncedSearchTerm]);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setQuery(values.query);
+  }
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)} className="flex items-center gap-2">
-      <FormControl>
-        <FormItem>
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex items-center gap-2">
           <FormField
             control={form.control}
             name="query"
-            render={({ field }) => <Input placeholder="File names" {...field} />}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input {...field} placeholder="search..." />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </FormItem>
-      </FormControl>
 
-      <Button size="sm" type="submit" disabled={form.formState.isSubmitting} className="flex gap-1">
-        {form.formState.isSubmitting && <Loader2 className="h-4 w-4 animate-spin" />}
-        <SearchIcon />
-        Search
-      </Button>
-    </form>
+          <Button
+            size="sm"
+            type="submit"
+            disabled={form.formState.isSubmitting}
+            className="hidden gap-1"
+            ref={submitButtonRef}
+          >
+            {form.formState.isSubmitting && <Loader2 className="h-4 w-4 animate-spin" />}
+            <SearchIcon /> Search
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 }
